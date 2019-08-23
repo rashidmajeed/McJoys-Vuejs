ebf7a4dc7e77af2ecd81f4d461d3c5dbbb2e3dcd
rashidmajeed <rashid.caliber@gmail.com>
2019-08-23 03:57:00 +0200

    added header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <appHeader />
+    <div>content</div>
+    <appFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appHeader from "./components/header-footer/Header";
+import appFooter from "./components/header-footer/Footer";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    appHeader,
+    appFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style.css";
 </style>
---
+++ src/components/header-footer/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div class="container clear">
+            <div class="logo">
+                <img :src="logoIcon">
+            </div>
+            <div class="nav">
+                <img :src="menuIcon">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+import Logo from '../../assets/images/mainlogo.png'
+import Menu from '../../assets/images/menu.png'
+
+export default {
+    data() {
+        return {
+            logoIcon: Logo,
+            menuIcon: Menu
+        }
+    }
+}
+</script>
